c50dfcd41bebb6a3858451bf45f241a52755d2b3
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-03 01:33:19 +0900

    interface definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,13 @@
 import styled, { keyframes } from "styled-components";
-
-const Wrapper = styled.div`
-  display: flex;
-`;
-
-const rotation = keyframes`
-  0%{
-    background-color: aqua;
-    transform: rotate(0deg);
-    border-radius: 50px;
-  }
-  50%{
-    background-color: tomato;
-    transform: rotate(180deg);
-    border-radius: 100px;
-  }
-  100%{
-    background-color: aquamarine;
-    transform: rotate(360deg);
-    border-radius: 50px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 170px;
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotation} 0.5s linear infinite;
-  ${Emoji}:hover {
-    font-size: 40px;
-  }
-`;
+import Circle from "./Circle";
 
 function App() {
   return (
-    <Wrapper>
-      <Box>
-        <Emoji>🤔</Emoji>
-      </Box>
-    </Wrapper>
+    <div>
+      <Circle bgColor="teal" />
+      <Circle bgColor="tomato" />
+      123
+    </div>
   );
 }
 
