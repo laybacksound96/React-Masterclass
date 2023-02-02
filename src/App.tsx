689a0876fafcf5f012cef832e4cd3c59595800e6
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-03 00:58:10 +0900

    First Typescript document

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotation = keyframes`
+  0%{
+    background-color: aqua;
+    transform: rotate(0deg);
+    border-radius: 50px;
+  }
+  50%{
+    background-color: tomato;
+    transform: rotate(180deg);
+    border-radius: 100px;
+  }
+  100%{
+    background-color: aquamarine;
+    transform: rotate(360deg);
+    border-radius: 50px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 170px;
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotation} 0.5s linear infinite;
+  ${Emoji}:hover {
+    font-size: 40px;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Box>
+        <Emoji>🤔</Emoji>
+      </Box>
+    </Wrapper>
   );
 }
 
