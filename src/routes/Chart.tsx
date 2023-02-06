cb403bbcaefd9b9a13d39e62dbff4add253212b5
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-07 02:40:51 +0900

    #5.16 - 차트 마무리

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,5 +1,82 @@
-function Chart() {
-  return <h1>Chart</h1>;
+import { useQuery } from "react-query";
+
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface CharProps {
+  coinId: string;
+}
+function Chart({ coinId }: CharProps) {
+  const { isLoading, data } = useQuery<IData[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading Chart..."
+      ) : (
+        // @ts-ignore
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: { mode: "dark" },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: { show: false },
+              background: "transparent",
+            },
+            stroke: {
+              curve: "smooth",
+              width: 5,
+            },
+            grid: {
+              show: false,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories: data?.map((price) =>
+                new Date(price.time_close * 1000).toUTCString()
+              ),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#4cd137"], stops: [0, 100] },
+            },
+            colors: ["#37d1bf"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$ ${value.toFixed(0)}`,
+              },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -6,7 +6,7 @@ import {
   useRouteMatch,
 } from "react-router";
 import styled from "styled-components";
-import { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import Chart from "./Chart";
 import Price from "./Price";
 import { Link } from "react-router-dom";
@@ -157,13 +157,19 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    { refetchInterval: 5000 }
   );
 
   const loading = infoLoading || tickersLoading;
 
   return (
     <Container>
+      <Helmet>
+        <title>
+          {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+        </title>
+      </Helmet>
       <Header>
         <Title>
           {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -183,8 +189,8 @@ function Coin() {
               <span>${infoData?.symbol}</span>
             </OverviewItem>
             <OverviewItem>
-              <span>Open Source:</span>
-              <span>{infoData?.open_source ? "Yes" : "No"}</span>
+              <span>Price:</span>
+              <span>{tickersData?.quotes.USD.price.toFixed(2)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
@@ -213,7 +219,7 @@ function Coin() {
               <Price />
             </Route>
             <Route path={`/${coinId}/chart`}>
-              <Chart />
+              <Chart coinId={coinId} />
             </Route>
           </Switch>
         </>
