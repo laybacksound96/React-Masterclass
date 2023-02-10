93a503e7160ad8f0248a726cf86006d74741b8d6
412 <laybacksound96@gmail.com>
2023-02-10 15:48:09 +0900

    config baseURL

--- src/api.ts
+++ src/api.ts
@@ -3,15 +3,23 @@ const BASE_URL = `https://api.coinpaprika.com/v1`;
 export function fetchCoins() {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 }
-export function fetchCoinInfo(coinId: string) {
-  return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
-    response.json()
-  );
+export async function fetchCoinInfo(coinId: string) {
+  const response = await fetch(`${BASE_URL}/coins/${coinId}`);
+  if (!response.ok) {
+    throw new Error("Error occured by fetching at fetchCoinInfo");
+  } else {
+    const data = await response.json();
+    return data;
+  }
 }
-export function fetchCoinTickers(coinId: string) {
-  return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
-    response.json()
-  );
+export async function fetchCoinTickers(coinId: string) {
+  const response = await fetch(`${BASE_URL}/tickers/${coinId}`);
+  if (!response.ok) {
+    throw new Error("Error occured by fetching at fetchCoinTickers");
+  } else {
+    const data = await response.json();
+    return data;
+  }
 }
 export async function fetchCoinHistory(coinId: string) {
   const response = await fetch(
